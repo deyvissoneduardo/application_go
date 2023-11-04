9af13f4fb10816f6cf8ac355ba7a46ba445a761c
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2023-11-04 00:00:13 -0300

    docker postgress and mod go init

--- main.go
+++ main.go
@@ -7,11 +7,23 @@ import (
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8181", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{"Iphone 15", "Apple", 6.500, 15},
+		{"Iphone 15 Plus", "Apple", 6.050, 10},
+		{"Iphone 15 Pro", "Apple", 7.030, 5},
+	}
+	temp.ExecuteTemplate(w, "Index", products)
 }
